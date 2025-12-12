18b14037ea8b6462c11164cb8de5a54e7802f92c
isaias tech <isaiasbatistapy.com>
2025-12-12 19:24:59 -0300

    :lock: criando pagina para entrar no sistema

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
-<?php 
-echo 'Olá, mundo!';
-?>
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <title>Login</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/login.css">
+</head>
+
+<body class="text-center">
+  <a href="views/">loteria</a>
+    <div class="form-signin bg-light">
+        <form>
+            <img class="mb-4" src="image/logo.png" alt="" width="150">
+            <h1 class="h3 mb-3 fw-normal">Faça login</h1>
+
+            <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email </label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Senha</label>
+            </div>
+
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Lembrar-me
+                </label>
+            </div>
+            <button class="w-100 btn btn-lg btn-dark" type="submit">Entrar</button>
+            <p class="mt-5 mb-3 text-muted">isaiasTech &copy; <?php echo date('Y')?></p>
+        </form>
+    </div>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
